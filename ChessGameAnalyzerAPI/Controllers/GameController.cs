24f2d0a8f7c49092cfe5c86b86f20a389430ae30
jorisreynes <joris.reynes81@gmail.com>
2023-05-08 21:59:31 +0200

    Cleaning

--- ChessGameAnalyzerAPI/Controllers/GameController.cs
+++ ChessGameAnalyzerAPI/Controllers/GameController.cs
@@ -13,7 +13,7 @@ namespace ChessGame_AnalyzerAPI.Controllers
     [ApiController]
     public class GameController : Controller
     {
-        private string pseudo { get; set; }
+        //private string pseudo { get; set; }
         
         // Files paths 
         private const string FilePathTxt = $@"../../DataSource/Text/data.txt";
@@ -23,46 +23,7 @@ namespace ChessGame_AnalyzerAPI.Controllers
         [HttpGet]
         public GamesResult GetGames(string? opening = "All openings", string? color= "All colors", string? endgame="All end of game")
         {
-            string firstMoves = "";
-            switch (opening)
-            {
-                case "Scotch":
-                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. d4";
-                    break;
-                case "Spanish":
-                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. Bb5";
-                    break;
-                case "Italian":
-                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. Bc4";
-                    break;
-                case "Alekhine":
-                    firstMoves = "1. e4 Cf6";
-                    break;
-                case "Sicilian":
-                    firstMoves = "1. e4 c5";
-                    break;
-                case "Caro-kann":
-                    firstMoves = "1. e4 c6";
-                    break;
-                case "Slav":
-                    firstMoves = "1. d4 d5 2. c4 c6";
-                    break;
-                case"Petrov":
-                    firstMoves = "1. e4 e5 2. Nf3 Nc6";
-                    break;
-                case "Scandinavian":
-                    firstMoves = "1. e4 d5";
-                    break;
-                case "e4":
-                    firstMoves = "1. e4";
-                    break;
-                case "d4":
-                    firstMoves = "1. d4";
-                    break;
-                case "All openings":
-                    firstMoves = "1.";
-                    break;
-            }
+            string firstMoves = FindFirstMoves(opening);
             
             switch (endgame)
             {
@@ -82,9 +43,76 @@ namespace ChessGame_AnalyzerAPI.Controllers
                     endgame = "";
                     break;
             }
+
+
+            List<ChessGame> games = CreateGamesList();
+            
+
+
+            string pseudo = FindPseudo(games);
             
             
             
+
+            // We create a list with all the games that contains the opening using LINQ
+            List<ChessGame> filteredGames = new List<ChessGame>();
+            
+            if (color == "White")
+            {
+                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
+                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.White == pseudo && g.Termination.Contains(endgame)).ToList();
+            }
+            else if(color == "Black")
+            {
+                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
+                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.Black == pseudo && g.Termination.Contains(endgame)).ToList();
+            }
+            else
+            {
+                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
+                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.Termination.Contains(endgame)).ToList();
+            }
+            
+            
+            // We save the games in a new XML file
+            PrintXml(games);
+            
+            // We save the games in a new JSON file
+            PrintJson(games);
+            
+            // We create a new GamesResult
+            GamesResult gamesResult = new GamesResult();
+            
+            // We score the games
+            foreach (ChessGame game in filteredGames)
+            {
+                if (game.Result == "1-0" && game.White == pseudo)
+                {
+                    gamesResult.NumberOfGamesWon++;
+                }
+                else if (game.Result == "1-0" && game.Black == pseudo)
+                {
+                    gamesResult.NumberOfGamesLost++;
+                }
+                else if (game.Result == "1/2-1/2")
+                {
+                    gamesResult.NumberOfGamesDrawn++;
+                }
+                else if (game.Result == "0-1" && game.Black == pseudo)
+                {
+                    gamesResult.NumberOfGamesWon++;
+                }
+                else if (game.Result == "0-1" && game.White == pseudo)
+                {
+                    gamesResult.NumberOfGamesLost++;
+                }
+            }
+            return gamesResult;
+        }
+
+
+        private static List<ChessGame> CreateGamesList()
+        {
             // We create a list avec all the games in the file from Chess.com
             List<ChessGame> games = new List<ChessGame>();
             using (StreamReader reader = new StreamReader(FilePathTxt))
@@ -155,7 +183,57 @@ namespace ChessGame_AnalyzerAPI.Controllers
                     }
                 }
             }
-            
+
+            return games;
+        }
+
+        private static string FindFirstMoves(string opening)
+        {
+            string firstMoves = "";
+            switch (opening)
+            {
+                case "Scotch":
+                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. d4";
+                    break;
+                case "Spanish":
+                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. Bb5";
+                    break;
+                case "Italian":
+                    firstMoves = "1. e4 e5 2. Nf3 Nc6 3. Bc4";
+                    break;
+                case "Alekhine":
+                    firstMoves = "1. e4 Cf6";
+                    break;
+                case "Sicilian":
+                    firstMoves = "1. e4 c5";
+                    break;
+                case "Caro-kann":
+                    firstMoves = "1. e4 c6";
+                    break;
+                case "Slav":
+                    firstMoves = "1. d4 d5 2. c4 c6";
+                    break;
+                case"Petrov":
+                    firstMoves = "1. e4 e5 2. Nf3 Nc6";
+                    break;
+                case "Scandinavian":
+                    firstMoves = "1. e4 d5";
+                    break;
+                case "e4":
+                    firstMoves = "1. e4";
+                    break;
+                case "d4":
+                    firstMoves = "1. d4";
+                    break;
+                case "All openings":
+                    firstMoves = "1.";
+                    break;
+            }
+            return firstMoves;
+        }
+
+        private static string FindPseudo(List<ChessGame> games)
+        {
             // function to find the pseudo of the player we have the most in white and black
             List<string> pseudoList = new List<string>();
             foreach (ChessGame game in games)
@@ -164,66 +242,10 @@ namespace ChessGame_AnalyzerAPI.Controllers
                 pseudoList.Add(game.Black);
             }
             // we find the pseudo that appears the most
-            pseudo = pseudoList.GroupBy(i => i).OrderByDescending(grp => grp.Count())
+            string pseudo = pseudoList.GroupBy(i => i).OrderByDescending(grp => grp.Count())
                 .Select(grp => grp.Key).First();
-
-            // We create a list with all the games that contains the opening using LINQ
-            List<ChessGame> filteredGames = new List<ChessGame>();
-            
-            if (color == "White")
-            {
-                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
-                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.White == pseudo && g.Termination.Contains(endgame)).ToList();
-            }
-            else if(color == "Black")
-            {
-                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
-                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.Black == pseudo && g.Termination.Contains(endgame)).ToList();
-            }
-            else
-            {
-                //we create a list with all the games that contains the opening, the color, and the endgame using LINQ
-                filteredGames = games.Where(g => g.Moves.Contains(firstMoves) && g.Termination.Contains(endgame)).ToList();
-            }
-            
-            
-            // We save the games in a new XML file
-            PrintXml(games);
-            
-            // We save the games in a new JSON file
-            PrintJson(games);
-            
-            // We create a new GamesResult
-            GamesResult gamesResult = new GamesResult();
-            
-            // We score the games
-            foreach (ChessGame game in filteredGames)
-            {
-                if (game.Result == "1-0" && game.White == pseudo)
-                {
-                    gamesResult.NumberOfGamesWon++;
-                }
-                else if (game.Result == "1-0" && game.Black == pseudo)
-                {
-                    gamesResult.NumberOfGamesLost++;
-                }
-                else if (game.Result == "1/2-1/2")
-                {
-                    gamesResult.NumberOfGamesDrawn++;
-                }
-                else if (game.Result == "0-1" && game.Black == pseudo)
-                {
-                    gamesResult.NumberOfGamesWon++;
-                }
-                else if (game.Result == "0-1" && game.White == pseudo)
-                {
-                    gamesResult.NumberOfGamesLost++;
-                }
-            }
-            return gamesResult;
+            return pseudo;
         }
-        
-        
         
         // Save the collection games in XML
         private static void PrintXml(IEnumerable<ChessGame> games)
